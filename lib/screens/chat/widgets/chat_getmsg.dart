b234a15b70ca1b948ae919ecc17e1f90a43042a9
HaeAhn <sejin.dong2000@gmail.com>
2025-08-10 03:06:30 +0900

    refactor: 시간 포맷 로직을 DateUtil로 분리

--- lib/screens/chat/widgets/chat_getmsg.dart
+++ lib/screens/chat/widgets/chat_getmsg.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project03/utils/app_theme.dart';
+import 'package:project03/utils/date_util.dart';
 
 class ChatGetMsg extends StatelessWidget {
   ChatGetMsg({
@@ -18,15 +19,6 @@ class ChatGetMsg extends StatelessWidget {
   final bool showTimestamp;
   @override
   Widget build(BuildContext context) {
-    // 시간을 '오전/오후 h:mm' 형식으로 변환하는 로직
-    String period = dateTime.hour < 12 ? '오전' : '오후';
-    int hour = dateTime.hour % 12;
-    if (hour == 0) {
-      hour = 12; // 0시는 12시로, 12시는 12시로 표시
-    }
-    String minute = dateTime.minute.toString().padLeft(2, '0');
-    final formattedTime = '$period $hour:$minute';
-
     // imgUrl이 비어있는지 확인하여 적절한 ImageProvider를 선택합니다.
     final ImageProvider profileImage = imgUrl.isNotEmpty
         ? NetworkImage(imgUrl)
@@ -80,7 +72,8 @@ class ChatGetMsg extends StatelessWidget {
               ),
               if (showTimestamp) const SizedBox(width: 8),
               if (showTimestamp)
-                Text(formattedTime, style: AppTextStyles.placeHolderText)
+                Text(DateUtil.formatMessageTime(dateTime),
+                    style: AppTextStyles.placeHolderText)
             ],
           ),
         ),
--- lib/screens/chat/widgets/chat_sendmsg.dart
+++ lib/screens/chat/widgets/chat_sendmsg.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project03/utils/app_theme.dart';
+import 'package:project03/utils/date_util.dart';
 
 class ChatSendMsg extends StatelessWidget {
   const ChatSendMsg({
@@ -13,22 +14,13 @@ class ChatSendMsg extends StatelessWidget {
   final bool showTimestamp;
   @override
   Widget build(BuildContext context) {
-    // 시간을 '오전/오후 h:mm' 형식으로 변환하는 로직
-    String period = dateTime.hour < 12 ? '오전' : '오후';
-    int hour = dateTime.hour % 12;
-    if (hour == 0) {
-      hour = 12; // 0시는 12시로, 12시는 12시로 표시
-    }
-    String minute = dateTime.minute.toString().padLeft(2, '0');
-    final formattedTime = '$period $hour:$minute';
-
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         if (showTimestamp)
           Text(
-            formattedTime,
+            DateUtil.formatMessageTime(dateTime),
             style: AppTextStyles.placeHolderText,
           ),
         if (showTimestamp) const SizedBox(width: 8),
