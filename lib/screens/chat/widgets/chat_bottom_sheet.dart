17e2657b0f28b9750b9f54d184789b04b9851ee6
HaeAhn <sejin.dong2000@gmail.com>
2025-08-10 00:57:33 +0900

    feat : 채팅기능 구현

--- lib/screens/chat/widgets/chat_bottom_sheet.dart
+++ lib/screens/chat/widgets/chat_bottom_sheet.dart
@@ -1,66 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:project03/utils/app_theme.dart';
+import 'package:project03/view_model/chat_view_model.dart';
 
-class ChatBottomSheet extends StatefulWidget {
-  ChatBottomSheet(this.bottomPadding);
-
+class ChatBottomSheet extends ConsumerStatefulWidget {
   final double bottomPadding;
+  const ChatBottomSheet(this.bottomPadding, {super.key});
+
   @override
-  State<ChatBottomSheet> createState() => _ChatBottomPageState();
+  ConsumerState<ChatBottomSheet> createState() => _ChatBottomSheetState();
 }
 
-class _ChatBottomPageState extends State<ChatBottomSheet> {
-  final controller = TextEditingController();
+class _ChatBottomSheetState extends ConsumerState<ChatBottomSheet> {
+  final TextEditingController _controller = TextEditingController();
 
   @override
   void dispose() {
-    controller.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
-  void onSend() {
-    print('onSend 터치됨');
+  void _sendMessage() {
+    // ViewModel을 통해 메시지 전송
+    ref.read(chatViewModelProvider.notifier).sendMessage(_controller.text);
+    // 입력창 비우기
+    _controller.clear();
+    // 키보드 내리기
+    FocusScope.of(context).unfocus();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 70 + widget.bottomPadding,
-      color: AppColors.secondary,
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
+      padding: EdgeInsets.fromLTRB(20, 10, 20, 10 + widget.bottomPadding),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+            offset: Offset(0, -2),
+          ),
+        ],
+      ),
+      child: Row(
         children: [
           Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: controller,
-                    onSubmitted: (value) => onSend(),
-                    decoration: InputDecoration(border:
-                        MaterialStateOutlineInputBorder.resolveWith((states) {
-                      return OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide(color: AppColors.primary));
-                    })),
-                  ),
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: '메시지를 입력하세요',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                  borderSide: BorderSide.none,
                 ),
-                GestureDetector(
-                  onTap: onSend,
-                  child: Container(
-                    width: 50,
-                    height: 50,
-                    color: Colors.transparent,
-                    child: Icon(
-                      Icons.send,
-                      color: AppColors.primary,
-                    ),
-                  ),
-                )
-              ],
+                filled: true,
+                fillColor: Colors.grey[200],
+                contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+              ),
+              onSubmitted: (value) => _sendMessage(),
             ),
           ),
-          SizedBox(height: widget.bottomPadding)
+          const SizedBox(width: 10),
+          IconButton(
+            icon: const Icon(Icons.send),
+            onPressed: _sendMessage,
+            color: AppColors.secondary,
+          ),
         ],
       ),
     );
--- lib/screens/chat/widgets/chat_content_page.dart
+++ lib/screens/chat/widgets/chat_content_page.dart
@@ -1,45 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:project03/models/chat_model.dart';
 import 'package:project03/screens/chat/widgets/chat_getmsg.dart';
 import 'package:project03/screens/chat/widgets/chat_sendmsg.dart';
 
 class ChatContentPage extends StatelessWidget {
+  final List<Chat> chats;
+  final String currentUserId;
+
+  const ChatContentPage({
+    super.key,
+    required this.chats,
+    required this.currentUserId,
+  });
+
   @override
   Widget build(BuildContext context) {
-    final children = [
-      ChatGetMsg(
-        imgUrl: 'https://picsum.photos/200/300',
-        showProfile: true,
-        name: '서연',
-        content: '안녕하세요',
-        dateTime: DateTime.now(),
-      ),
-      ChatGetMsg(
-        imgUrl: 'https://picsum.photos/200/300',
-        showProfile: false,
-        name: '서연',
-        content: '잘 지내시나요?',
-        dateTime: DateTime.now(),
-      ),
-      ChatSendMsg(
-        name: '이샘',
-        content: '안녕하세요',
-        dateTime: DateTime.now(),
-      ),
-      ChatSendMsg(
-        name: '이샘',
-        content: '잘 지내고 있습니다!',
-        dateTime: DateTime.now(),
-      ),
-    ];
-    return Expanded(
-      child: ListView.separated(
-        itemCount: children.length,
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        separatorBuilder: (context, index) => SizedBox(height: 10),
-        itemBuilder: (context, index) {
-          return children[index];
-        },
-      ),
+    // 새 메시지가 오면 맨 아래로 스크롤하기 위한 컨트롤러
+    final ScrollController scrollController = ScrollController();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (scrollController.hasClients) {
+        scrollController.jumpTo(scrollController.position.maxScrollExtent);
+      }
+    });
+
+    return ListView.separated(
+      controller: scrollController,
+      // 하단 입력창에 가려지지 않도록 아래쪽에 여백 추가
+      padding: const EdgeInsets.fromLTRB(20, 10, 20, 90),
+      itemCount: chats.length,
+      separatorBuilder: (context, index) => const SizedBox(height: 10),
+      itemBuilder: (context, index) {
+        final chat = chats[index];
+        final isMe = chat.senderId == currentUserId;
+
+        if (isMe) {
+          return ChatSendMsg(
+            name: chat.sender,
+            content: chat.message,
+            dateTime: chat.timestamp,
+          );
+        } else {
+          // 같은 사람이 보낸 연속된 메시지에서는 프로필을 한 번만 보여주기 위한 로직
+          final bool showProfile =
+              index == 0 || chats[index - 1].senderId != chat.senderId;
+          return ChatGetMsg(
+            imgUrl: chat.profileImgUrl ?? '', // 프로필 이미지가 없으면 빈 문자열 전달
+            showProfile: showProfile,
+            name: chat.sender,
+            content: chat.message,
+            dateTime: chat.timestamp,
+          );
+        }
+      },
     );
   }
 }
